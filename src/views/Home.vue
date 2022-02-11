ed79355cad0de4f88ff6257e4591b694dd4c2e39
João Reis <joaoreisjpk@gmail.com>
2022-02-11 14:38:49 -0300

    creating the home file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="splash-container">
+        <div class="splash">
+          <h1>Splendid Food</h1>
+        </div>
+      </div>
+
+      <main class="wrapper">
+        <h2>Recommended</h2>
+
+        <div class="recommended">
+
+          <div v-for="product in inventory.slice(0,3)" :key="product.id" class="card">
+            <div class="card-title">{{ product.name }}</div>
+            <div class="card-body">
+              <i class="icofont-10x icofont-{{ product.icon }}"></i>
+              <form>
+                <div class="row">
+                  <div class="cell">
+                    <label>Type:</label>
+                  </div>
+                  <div class="cell">
+                    <em>Fruit</em>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="cell">
+                    <label>Price:</label>
+                  </div>
+                  <div class="cell">${{ product.price.USD }}</div>
+                </div>
+                <div class="row">
+                  <div class="cell">
+                    <label>Quantity:</label>
+                  </div>
+                  <div class="cell">
+                    <input type="number" v-model="product.quantity" />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-light" @click="addToCart(product.name)">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <sidebar v-if="showSidebar" :toggle="toggleSidebar" :cart="cart" />
+      </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import food from '../food.json'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      inventory: food
+    }
+  },
+  components: {}
 }
 </script>
